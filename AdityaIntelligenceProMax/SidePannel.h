4329b78372fdebbda08deb5054cf0ef37f4e6efd
Aditya Dudeja <aditya.a75.dudeja@gmial.com>
2026-02-26 20:09:20 +0530

    Change L3: Intelligence 1. intelligence is a data processing engine and pipeline that utilises MatrixH and Renderer to process data at a high throghput and low latency 2. It also has a User Interface option were we can add sliders. MatrixH 1. Minor Bug fixes

--- AdityaIntelligenceProMax/SidePannel.h
+++ AdityaIntelligenceProMax/SidePannel.h
@@ -9,18 +9,45 @@
 #define SidePannel_h
 #include <vector>
 #include <memory>
-
+#if !TARGET_OS_IPHONE
 @interface FlippedStackView : NSStackView
 @end
 
 @interface SidePannel : NSViewController
 @property (nonatomic, strong) FlippedStackView* Vstack;
-- (instancetype)initWithVector:(std::vector<std::shared_ptr<GeometryNode<uint16>>>*)vec;
+- (instancetype)initWithVector:(std::vector<std::shared_ptr<GeometryNode<uint16_t>>>*)vec;
 - (void) updateAssetManager;
 @end
 
 @interface Benchmark : NSViewController
 
 @end
+#endif
 
+
+
+
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+
+// Forward declare your C++ class if needed
+// template <typename T> class GeometryNode;
+
+// 1. Benchmark is just a UIViewController
+@interface Benchmark : UIViewController
+@end
+
+// 2. SidePannel
+@interface SidePannel : UIViewController
+
+
+@property (nonatomic, strong) UIStackView* Vstack;
+
+// Constructor signature remains identical (C++ works fine here)
+- (instancetype)initWithVector:(std::vector<std::shared_ptr<GeometryNode<uint16_t>>>*)vec;
+
+- (void) updateAssetManager;
+
+@end
+#endif
 #endif /* SidePannel_h */
